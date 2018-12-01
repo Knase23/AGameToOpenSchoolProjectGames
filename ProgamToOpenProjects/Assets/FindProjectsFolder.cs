b49a130b80b91e91bc81db5d0cee07257202c765
Knase23 <Knase23@users.noreply.github.com>
2018-12-01 16:14:20 +0100

    Added unity Project

---
+++ ProgamToOpenProjects/Assets/FindProjectsFolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FindProjectsFolder : MonoBehaviour {
+
+    public string customFolderName = "Projects";
+    public string executableLocation;
+    public string customFolderPath;
+    
+	// Use this for initialization
+	void Start () {
+
+        bool existsProjectsFolder = false;
+        executableLocation = Directory.GetCurrentDirectory();
+
+        string[] folders = Directory.GetDirectories(executableLocation);
+    
+        for (int i = 0; i < folders.Length; i++)
+        {
+            //Removes the text that is our Current Directory
+            folders[i] = folders[i].Remove(0, executableLocation.Length + 1);
+
+            // Make sure we have spesific folder
+            if(folders[i] == customFolderName)
+            {
+                existsProjectsFolder = true;
+                //Debug.Log("Projects Exists");
+                customFolderPath = executableLocation + @"\" + customFolderName;
+            } 
+        }
+
+        // Create the spesific folder if it does not exists
+        if (!existsProjectsFolder)
+        {
+            Directory.CreateDirectory(executableLocation + @"\"+ customFolderName);
+            //Debug.Log("Projects is Created");
+            customFolderPath = executableLocation + @"\" + customFolderName;
+        }
+    }
+    public string GetProjectsFolderPath()
+    {
+        return customFolderPath;
+    }
+}
---
+++ ProgamToOpenProjects/Assets/FoldersInProjects.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(FindProjectsFolder))]
+public class FoldersInProjects : MonoBehaviour {
+
+    private string projectsPath;
+    public string[] folders;
+	// Use this for initialization
+	void Start () {
+        projectsPath = GetComponent<FindProjectsFolder>().GetProjectsFolderPath();
+        folders = CheckFolderNames();
+	}
+	
+	void Update () {
+        folders = CheckFolderNames();
+	}
+
+    private string[] CheckFolderNames()
+    {
+        string[] f = Directory.GetDirectories(projectsPath);
+
+        for (int i = 0; i < f.Length; i++)
+        {
+            //Removes the text that is our Current Directory
+            f[i] = f[i].Remove(0, projectsPath.Length + 1);
+        }
+
+        return f;
+    }
+    /// <summary>
+    /// Gives the Fullpath of a folder
+    /// </summary>
+    public string GetFolderFullPath(int positionInArray)
+    {
+        if(positionInArray < 0 || positionInArray > folders.Length - 1)
+        {
+            throw new System.ArgumentException("Is not inside the array", "positionInArray");
+        }
+        return projectsPath + @"\" + folders[positionInArray];
+    }
+}
